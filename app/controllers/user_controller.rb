a564a7f1ab9c311156a3b243fc0a491a6b8a06c7
SeoDaeHoon <gomme9396@gmail.com>
2016-12-28 16:39:09 +0900

    로그인 디자인 수정

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,8 +1,8 @@
 class UserController < ApplicationController
-    
+
     def join
     end
-    
+
     # 회원가입
     def join_path
         a = User.where(:email => params[:email]).take
@@ -15,58 +15,58 @@ class UserController < ApplicationController
             redirect_to '/user/join_error'
         end
     end
-    
+
     def login
     end
-    
+
     # 로그인
     def login_path
         u = User.where(:email => params[:email]).take
         unless u.nil?
             session[:user_id] = u.email
-            redirect_to '/'
+            redirect_to '/review/list'
         else
             redirect_to '/user/login_error'
         end
     end
-    
+
     # 로그아웃
     def logout
         reset_session
         redirect_to '/'
     end
-    
+
     # 회원 정보 수정
     def edit
         @one_user = User.find(params[:id])
     end
-    
+
     def edit_path
         @one_user = User.find(params[:id])
         @one_review = Review.where(:writer => @one_user.email).all
-        
+
         a = User.where(:email => params[:email]).take
-        
+
         if a.nil?
             @one_review.each do |r|
                 r.writer = params[:email]
                 r.save
             end
-            
+
             @one_user.email = params[:email]
-            
+
             @one_user.save
-            
+
             redirect_to '/user/login'
         else
             redirect_to '/user/join_error'
         end
-        
+
     end
-    
+
     def data
         @user = User.all
         @review = Review.all
     end
-    
+
 end
