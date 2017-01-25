41329061c42bbdc910eb61b2f31eb7f17a963d9e
rlsvkf1012 <rlsvkf1012@naver.com>
2017-01-25 08:13:57 +0000

    댓글 모델 생성

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170123064057) do
+ActiveRecord::Schema.define(version: 20170125081035) do
 
   create_table "boards", force: :cascade do |t|
     t.string   "parcel_address"
     t.string   "writer"
     t.string   "title"
+    t.string   "content"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "parcel_address"
+    t.string   "writer"
     t.string   "content"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
